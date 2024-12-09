b660dbdb846b765f4ce5b2b3340a2f34ab0eabfc
Сергей Коновалов <SokiZNosoK68rus@yandex.ru>
2024-12-09 19:39:44 +0300

    polls/models updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,13 +14,13 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
 ]
 
 MIDDLEWARE = [
