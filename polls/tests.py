d616dc652f5bf8ac5e98d64d7dfc9680a337fc2d
Сергей Коновалов <SokiZNosoK68rus@yandex.ru>
2024-12-21 16:30:46 +0300

    QuestionResultsViewTest, ResultsView.get_queryset() added

--- polls/tests.py
+++ polls/tests.py
@@ -117,7 +117,19 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in the past
         displays the question's text.
         """
-        past_question = create_question(question_text="Past Question.", days=-5)
+        past_question = create_question(question_text="Past question.", days=-5)
         url = reverse("polls:detail", args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
+
+
+class QuestionResultsViewTest(TestCase):
+    def test_future_question(self):
+        """
+        The results view of a question wiyh a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:results", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
